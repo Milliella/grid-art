06908e750a95a0e0e69390135cd6a5bea3fbfeb0
Milliella <115160750+Milliella@users.noreply.github.com>
2023-01-17 12:26:03 +0000

    grid art morning + images

--- base-p5-project-main/js/grid2.js
+++ base-p5-project-main/js/grid2.js
@@ -1,45 +1,31 @@
+let textures = [];
+function preload(){
+    let texturesNum = 4;
+    for(let i = 1; i <= texturesNum;i++){
+      //  let texture = loadImage("images/artistcanvas"+i+".jpg"); //concatenation
+      let t = loadImage(`images/artistcanvas${i}.jpg`); //backticks are template literals and dollar sign curly brace is string interpolation
+      textures.push(t); //adds texture to textures array
 
-function setup() {
-    const CELL_SIZE = 40;
-    createCanvas(800, 800);
-    background(255);
-    //noStroke();
-    let x = 0;
-    let y = 0;
-    let xVariation = 5;
-    let yVariation = 10;
-    let widthVariation = 10;
-    let heightVariation = 20;
-    let sizes = [CELL_SIZE, CELL_SIZE * 2, CELL_SIZE * 3];
-    let currentWidth = random(sizes);
-    let currentHeight = random(sizes);
-    let colours = [color('#a9a6ab'), color('#bc3917'), color('#1b1f2a'), color('#d8a91d')];
-    strokeWeight(5);
-    while (y < height) {
-        while (x < width) {
-            fill(random(colours));
-            // rect(x+random(-xVariation, xVariation),y + random(-yVariation, yVariation),
-            // CELL_SIZE + random(-widthVariation, widthVariation), CELL_SIZE + random
-            // (-heightVariation, heightVariation));
-            rect(x, y, currentWidth, currentHeight);
-            x += currentWidth;
-            let availableWidth = width - x;
-            currentWidth = random(sizes);
-            while (currentWidth > availableWidth) {
-                currentWidth -= CELL_SIZE;
-            }
-
-        }
-
-        y += currentHeight;
-        let availableHeight = height - y;
-        currentHeight = random(sizes);
-        while (currentHeight > availableHeight) {
-            currentHeight -= CELL_SIZE;
-        }
-        x = 0;
     }
+
 }
-function draw() {
+function setup() {
+    createCanvas (800, 800);
+    const CELL_SIZE = 80;
+    let x =0;
+    let y =0;
+    noStroke()
+    while(y < height){
+        while(x < width){
+            let t = random(textures);
+            image(t, x, y, CELL_SIZE)
+            x+=CELL_SIZE;
+
+        }
+        y+=CELL_SIZE;
+        x =0;
+
+    }
+    
 }
 
