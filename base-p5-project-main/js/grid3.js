b9ea2c23c39e75e0ec1fc1ac2763930e755daef0
Milliella <115160750+Milliella@users.noreply.github.com>
2023-01-17 15:16:11 +0000

    afternoon

--- base-p5-project-main/js/grid3.js
+++ base-p5-project-main/js/grid3.js
@@ -1,26 +1,23 @@
 function setup() {
     const CELL_SIZE = 40;
-    createCanvas(1500, 1500);
+    createCanvas(1200, 1200);
     background(255);
     //noStroke();
     let x = 0;
     let y = 0;
     let xVariation = 5;
-    let yVariation = 10;
-    let widthVariation = 10;
-    let heightVariation = 20;
+    let yVariation = 5;
+    let widthVariation = 5;
+    let heightVariation = 5;
     let sizes = [CELL_SIZE, CELL_SIZE * 2, CELL_SIZE * 3];
     let currentWidth = random(sizes);
     let currentHeight = random(sizes);
     let colours = [color(`#D81E5B`), color('#bc3917'), color('#1b1f2a'), color('#d8a91d')];
-    strokeWeight(5);
+    //strokeWeight(5);
     while (y < height) {
         while (x < width) {
             fill(random(colours));
-            // rect(x+random(-xVariation, xVariation),y + random(-yVariation, yVariation),
-            // CELL_SIZE + random(-widthVariation, widthVariation), CELL_SIZE + random
-            // (-heightVariation, heightVariation));
-            rect(x, y, currentWidth, currentHeight);
+            ellipse(x, y, currentWidth, currentHeight);
             x += currentWidth;
             let availableWidth = width - x;
             currentWidth = random(sizes);
